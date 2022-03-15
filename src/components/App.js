8cbc687ce58560654fa7b57420369f695cbefaad
crocodeev <svin862003@mail.ru>
2022-03-15 12:27:15 +0300

    first commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,33 @@
 import React from 'react'
 
 import '../assets/css/App.css'
+import fs from "fs/promises"
+import bufferToDataUrl from "buffer-to-data-url"
+import { Howler, Howl } from "howler"
+
+/*
+async function play(){
+  const data = await fs.readFile("../music/audio.mp3");
+  console.log(data);
+}
+
+play();*/
+
+fs.readFile("C:\\Users\\AK\\Music\\audio.mp3")
+.then(data => {
+  console.log(data);
+  const URL = bufferToDataUrl("music", data);
+  return URL;
+})
+.then(url => {
+  const sound = new Howl({
+    src: [ url ],
+    format: ["mp3"]
+  })
+
+  sound.play();
+})
+.catch(e => console.log(e))
 
 function App() {
   return (
